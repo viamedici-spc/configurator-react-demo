a412db50647fa31f9aa7c1bd143a81a0caa2a611
birmitt <13151231+birmitt@users.noreply.github.com>
2024-05-30 14:32:17 +0200

    Renamed wwwroot to src

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
 export default defineConfig(({command, mode}) => ({
-    root: "wwwroot",
+    root: "src",
     server: {
         port: 3000
     },
